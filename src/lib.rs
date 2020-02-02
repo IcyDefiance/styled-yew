14924d18e614e8dea07c28a597b9265304f77f7b
IcyDefiance <icydefiance@gmail.com>
2020-02-02 09:34:40 -0500

    Remove code that polls for a node ref

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,12 @@ use lazy_static::lazy_static;
 use std::{
 	any::TypeId,
 	collections::HashMap,
-	sync::{atomic::AtomicUsize, RwLock},
+	sync::{atomic::AtomicI32, Mutex},
 };
 
-pub static NEXT_ID: AtomicUsize = AtomicUsize::new(0);
+pub static NEXT_ID: AtomicI32 = AtomicI32::new(0);
 lazy_static! {
-	pub static ref STYLE_STATES: RwLock<HashMap<TypeId, usize>> = RwLock::default();
+	pub static ref STYLE_IDS: Mutex<HashMap<TypeId, i32>> = Mutex::default();
 }
 
 #[macro_export]
@@ -21,56 +21,40 @@ macro_rules! styled {
 			// TODO: store instance of child component and proxy each method to child, instead of sending props to child
 			// in the view method
 			props: <$child as Component>::Properties,
-			// TODO: node_ref is unavailable inside mounted() and in the first update() call, so we have to call update
-			// until it is available. this feels dirty and inefficient.
-			link: ComponentLink<Self>,
 			node_ref: ::yew::NodeRef,
-			added_class: bool,
 		}
 		impl Component for $name {
 			type Message = ();
 			type Properties = <$child as Component>::Properties;
 
 			fn create(props: Self::Properties, mut link: ComponentLink<Self>) -> Self {
-				use std::sync::Mutex;
 				use yew::NodeRef;
 
-				Self { props, link, node_ref: NodeRef::default(), added_class: false }
+				Self { props, node_ref: NodeRef::default() }
 			}
 
 			fn mounted(&mut self) -> ShouldRender {
-				use $crate::{stdweb::js, STYLE_STATES};
+				use $crate::{stdweb::{js, web::Element}, STYLE_IDS};
 				use std::{any::TypeId, sync::atomic::Ordering};
 
-				let typeid = TypeId::of::<Self>();
-				if !STYLE_STATES.read().unwrap().contains_key(&typeid) {
+				let id = *STYLE_IDS.lock().unwrap().entry(TypeId::of::<Self>()).or_insert_with(|| {
 					let id = $crate::NEXT_ID.fetch_add(1, Ordering::Relaxed);
 					let rules = concat!($( $crate::css_rule!($rule), ":", $val, ";" )*);
 					js! {
 						const style = document.createElement("style");
 						document.head.appendChild(style);
-						style.sheet.insertRule(".sc" + @{id.to_string()} + "{" + @{rules} + "}");
+						style.sheet.insertRule(".sc" + @{id} + "{" + @{rules} + "}");
 					}
-					STYLE_STATES.write().unwrap().insert(typeid, id);
-				}
+					id
+				});
 
-				self.link.send_message(());
+				let el = self.node_ref.try_into::<Element>().unwrap();
+				js! { @{el}.className += " sc" + @{id}; };
+
 				false
 			}
 
-			fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-				use $crate::{STYLE_STATES, stdweb::{js, web::Element}};
-				use std::any::TypeId;
-
-				if !self.added_class {
-					if let Some(el) = self.node_ref.try_into::<Element>() {
-						let id = *STYLE_STATES.read().unwrap().get(&TypeId::of::<Self>()).unwrap();
-						js! { @{el}.className += " sc" + @{id.to_string()}; };
-						self.added_class = true;
-					} else {
-						self.link.send_message(());
-					}
-				}
+			fn update(&mut self, _: Self::Message) -> ShouldRender {
 				false
 			}
 
